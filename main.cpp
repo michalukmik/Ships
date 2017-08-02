b91f78da444991bbd81d51190f2d66a726586ed5
michalukmik <michalukmikolaj@gmail.com>
2017-08-02 13:31:34 +0200

    Rysowanie mapy

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class pole{
+private:
+
+
+public:
+
+
+
+};
+
 int main()
 {
-    int a=2;
+
+int tab[9][9];
+for(int i=0;i<9;i++)
+    for(int j=0;j<9;j++)
+    {
+    tab[i][j]=0;
+    }
+for(int i=6;i<7;i++)
+    for(int j=2;j<5;j++)
+    {
+    tab[i][j]=1;
+    }
+
+for(int i=0;i<9;i++)
+{
+    for(int j=0;j<9;j++)
+    {
+    if(tab[i][j]==0) cout << " . ";
+    else if(tab[i][j]==1) cout << " X ";
+    }
+    cout << endl;
+}
 
 
-    cout << a;
     return 0;
 }
