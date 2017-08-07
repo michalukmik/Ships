971f5e57e4c2bd74d2d7e902f9b419f80a5904ef
michalukmik <michalukmikolaj@gmail.com>
2017-08-07 17:28:49 +0200

    Dodanie doxygen/doxyfile

--- main.cpp
+++ main.cpp
@@ -1,42 +1,42 @@
+/** \brief Celem projektu jest budowa dwu osobowej gry w statki
+ *    Ka¿dy z graczy widzi przed sob¹ dwie planszê. Jedna jest ods³oniêta,
+ * gracz widzi na niej swoje statki i ostatnie dwa strza³y przeciwnika.
+ *Druga plansza jest zas³oniêta, gracz widzi na niej swoje strza³y jako pud³a, trafieniai i zatopienia statków przeciwnika.
+ *
+ *Plansz ma wymiary 8x8. U³o¿enie statków, jak i rodziaj jest losowy,a ponadto statki nie mog¹ znajdowaæ siê na polach przyleg³ych.
+ *
+ *W grzê maj¹ byæ co najmniej 3 rodzaje statków posiadaj¹cych mo¿liwoœæ ruchu w p³asczyznie pocz¹tkowej(o ustalon¹ liczbe pól)
+ *oraz mo¿liwoœæ strza³u. Statek co najmniej raz trafiony jest wyeliminowany z gry.
+ *
+ *Ruch ka¿dego gracza dzieli siê na nastêpuj¹ce fazy:
+ *1.Nasisniêcie Enter
+ *2.Narysowanie mapy
+ *3.Wybór statku
+ *4.Ruch, tzn. przeuniêcie i strza³
+ *5.Wynik strza³u
+ *6.Nacisniêcie Enter
+ *7.Zmazanie mapy
+ *8.Punkt 1. dla drugiego gracza
+ *
+ * Gra koñczy siê, gdy gracz straci wszystkie statki lub straci mo¿liwoœæ strza³u.
+ * \return
+ *
+ */
+
+
+
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-class pole{
-private:
 
-
-public:
-
-
-
-};
 
 int main()
 {
 
-int tab[9][9];
-for(int i=0;i<9;i++)
-    for(int j=0;j<9;j++)
-    {
-    tab[i][j]=0;
-    }
-for(int i=6;i<7;i++)
-    for(int j=2;j<5;j++)
-    {
-    tab[i][j]=1;
-    }
 
-for(int i=0;i<9;i++)
-{
-    for(int j=0;j<9;j++)
-    {
-    if(tab[i][j]==0) cout << " . ";
-    else if(tab[i][j]==1) cout << " X ";
-    }
-    cout << endl;
-}
 
 
     return 0;
