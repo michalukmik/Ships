0d5d52d6db3690e0d34c89a547931ac5e1374c14
michalukmik <michalukmikolaj@gmail.com>
2017-08-18 16:27:30 +0200

    zmianna

--- main.cpp
+++ main.cpp
@@ -1,42 +1,22 @@
-/** \brief Celem projektu jest budowa dwu osobowej gry w statki
- *    Ka¿dy z graczy widzi przed sob¹ dwie planszê. Jedna jest ods³oniêta,
- * gracz widzi na niej swoje statki i ostatnie dwa strza³y przeciwnika.
- *Druga plansza jest zas³oniêta, gracz widzi na niej swoje strza³y jako pud³a, trafieniai i zatopienia statków przeciwnika.
- *
- *Plansz ma wymiary 8x8. U³o¿enie statków, jak i rodziaj jest losowy,a ponadto statki nie mog¹ znajdowaæ siê na polach przyleg³ych.
- *
- *W grzê maj¹ byæ co najmniej 3 rodzaje statków posiadaj¹cych mo¿liwoœæ ruchu w p³asczyznie pocz¹tkowej(o ustalon¹ liczbe pól)
- *oraz mo¿liwoœæ strza³u. Statek co najmniej raz trafiony jest wyeliminowany z gry.
- *
- *Ruch ka¿dego gracza dzieli siê na nastêpuj¹ce fazy:
- *1.Nasisniêcie Enter
- *2.Narysowanie mapy
- *3.Wybór statku
- *4.Ruch, tzn. przeuniêcie i strza³
- *5.Wynik strza³u
- *6.Nacisniêcie Enter
- *7.Zmazanie mapy
- *8.Punkt 1. dla drugiego gracza
- *
- * Gra koñczy siê, gdy gracz straci wszystkie statki lub straci mo¿liwoœæ strza³u.
- * \return
+/** \brief Gra w statki dla dwoch osob.
+ *Gra w statki dla dwoch osob. W grze wystepuja rozne typy statkow, ktore moga sie przemieszczaczacz i strzelac.
+ *  Raz trafiony statek jest unieszkodliwiony, tzn. nie moze wykonywac ruchu, ale zajmiuje teren.
+ * Statki nie mogÄ… znajdowaÄ‡ siÄ™ na sasiadujacych polach.
+ * Ruch gracza polega na wybraniu statku i w zaleznosci od typu przemieszczenie ,a nastepnie oddanie strzalu.
+ *Gra konczy sie jak ktorys z graczy straci wszytskie statki.
  *
  */
 
 
 
 
+
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-
-
 int main()
 {
-
-
 
 
     return 0;
